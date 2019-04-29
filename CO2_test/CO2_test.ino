98595cbd11c09f5d2b74528ecc0c7a9e64c173a9
asad26 <asad26@users.noreply.github.com>
2019-04-29 11:28:35 +0100

    Update CO2_test.ino

--- CO2_test/CO2_test.ino
+++ CO2_test/CO2_test.ino
@@ -1,15 +1,6 @@
-/*!
- * @file  DFRobot_SHT20_test.ino
- * @brief DFRobot's SHT20 Humidity And Temperature Sensor Module
- * @n     This example demonstrates how to read the user registers to display resolution and other settings.
- *        Uses the SHT20 library to display the current humidity and temperature.
- *        Open serial monitor at 9600 baud to see readings.
- *        Errors 998 if not sensor is detected. Error 999 if CRC is bad.
- * Hardware Connections:
- * -VCC = 3.3V
- * -GND = GND
- * -SDA = A4 (use inline 330 ohm resistor if your board is 5V)
- * -SCL = A5 (use inline 330 ohm resistor if your board is 5V)
+/*
+ * Hardware connections:
+ *  - S-100 TX line to ESP8266 RX line
  */
 
 #include <ESP8266WiFi.h>
