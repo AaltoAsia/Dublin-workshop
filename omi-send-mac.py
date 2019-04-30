2572e88e671891bf61d69553307ddce28db38239
Asad Javed <asad.javed@aalto.fi>
2019-04-30 16:04:19 +0100

    Added script for MAC

---
+++ omi-send-mac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import requests
+
+while True:
+   requests.post("http://localhost:8080", data =
+"""<omiEnvelope xmlns="http://www.opengroup.org/xsd/omi/1.0/" version="1.0" ttl="0">
+ <write msgformat="odf">
+   <msg>
+     <Objects xmlns="http://www.opengroup.org/xsd/odf/1.0/">
+       <Object>
+         <id>Example</id>
+         <InfoItem name="userInput">
+           <value>""" + input().replace('°C','') + """</value>
+         </InfoItem>
+       </Object>
+     </Objects>
+   </msg>
+ </write>
+</omiEnvelope>""")
