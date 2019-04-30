21121ec71f3120c9119f84c46c9a234717efe920
asad26 <asad26@users.noreply.github.com>
2019-04-30 14:52:16 +0100

    Create omi-agent.py

---
+++ omi-agent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+
+while True:
+    requests.post("http://localhost:8080", data =
+"""<omiEnvelope xmlns="http://www.opengroup.org/xsd/omi/1.0/" version="1.0" ttl="0">
+  <write msgformat="odf">
+    <msg>
+      <Objects xmlns="http://www.opengroup.org/xsd/odf/1.0/">
+        <Object>
+          <id>Computer</id>
+          <InfoItem name="CPUTemperature">
+            <value>""" + input() + """</value>
+          </InfoItem>
+        </Object>
+      </Objects>
+    </msg>
+  </write>
+</omiEnvelope>""")
