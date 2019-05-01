8b6f7d5b25c3a8018b22081eadf7de1ce4256361
asad26 <asad26@users.noreply.github.com>
2019-05-01 10:36:49 +0100

    Update omi-send-mac.py

--- omi-send-mac.py
+++ omi-send-mac.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 import requests
 
-while True:
-   requests.post("http://localhost:8080", data =
+r = requests.post("http://localhost:8080", data =
 """<omiEnvelope xmlns="http://www.opengroup.org/xsd/omi/1.0/" version="1.0" ttl="0">
  <write msgformat="odf">
    <msg>
      <Objects xmlns="http://www.opengroup.org/xsd/odf/1.0/">
        <Object>
          <id>MAC-Computer</id>
-         <InfoItem name="CPUTemperature">
+         <InfoItem name="CPU-Temperature">
            <value>""" + input().replace('°C','') + """</value>
          </InfoItem>
        </Object>
@@ -17,3 +16,4 @@ while True:
    </msg>
  </write>
 </omiEnvelope>""")
+print(r.text)
